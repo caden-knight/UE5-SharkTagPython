1e22155a3ad3235090dfc8bcf00fb01053387551
Caden Morrison <64432691+caden-knight@users.noreply.github.com>
2023-07-19 18:57:25 -0600

    added atmospherics to level

--- addAtmosphere.py
+++ addAtmosphere.py
@@ -1,1 +1,15 @@
 import unreal
+# define actor location
+actorLocation = unreal.Vector(0, 0, 0)
+actorRotation = unreal.Rotator(0, 0, 0)
+levelSubsys = unreal.get_editor_subsystem(unreal.LevelEditorSubsystem)
+
+# create new atmospheric actors
+dirLight = unreal.EditorLevelLibrary.spawn_actor_from_class(unreal.DirectionalLight, actorLocation, actorRotation, False)
+skyLight = unreal.EditorLevelLibrary.spawn_actor_from_class(unreal.SkyLight, actorLocation, actorRotation, False)
+sky = unreal.EditorLevelLibrary.spawn_actor_from_class(unreal.SkyAtmosphere, actorLocation, actorRotation, False)
+fog = unreal.EditorLevelLibrary.spawn_actor_from_class(unreal.ExponentialHeightFog, actorLocation, actorRotation, False)
+clouds = unreal.EditorLevelLibrary.spawn_actor_from_class(unreal.VolumetricCloud, actorLocation, actorRotation, False)
+
+# save level
+levelSubsys.save_current_level()
